3ac6c40dd731be2f6783c02ab3f392a7483f192d
Sourabh0777 <sourabh.verma9770@gmail.com>
2025-06-08 22:49:10 +0530

    UI: nav bar nested submenu showing added

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -137,19 +137,64 @@ const Header = () => {
                               </span>
                             </p>
                             <div
-                              className={`submenu dark:bg-dark relative top-full left-0 rounded-sm bg-white transition-[top] duration-300 group-hover:opacity-100 lg:invisible lg:absolute lg:top-[110%] lg:block lg:w-[250px] lg:p-4 lg:opacity-0 lg:shadow-lg lg:group-hover:visible lg:group-hover:top-full ${
-                                openIndex === index ? "block" : "hidden"
-                              }`}
+                              className="submenu dark:bg-dark relative top-full left-0 rounded-sm bg-white transition-[top] duration-300 group-hover:opacity-100 lg:invisible lg:absolute lg:top-[110%] lg:block lg:w-[250px] lg:p-4 lg:opacity-0 lg:shadow-lg lg:group-hover:visible lg:group-hover:top-full"
                             >
-                              {menuItem.submenu.map((submenuItem, index) => (
-                                <Link
-                                  href={submenuItem.path}
-                                  key={index}
-                                  className="text-dark hover:text-primary block rounded-sm py-2.5 text-sm lg:px-3 dark:text-white/70 dark:hover:text-white"
-                                >
-                                  {submenuItem.title}
-                                </Link>
-                              ))}
+                              {menuItem.submenu.map((submenuItem, index) => {
+                                if (submenuItem.submenu) {
+                                  return (
+                                    <div key={index} className="relative group/submenu">
+                                      <p className="text-dark hover:text-primary block rounded-sm py-2.5 text-sm lg:px-3 dark:text-white/70 dark:hover:text-white cursor-pointer flex items-center justify-between">
+                                        {submenuItem.title}
+                                        <span className="pl-3">
+                                          <svg
+                                            width="25"
+                                            height="24"
+                                            viewBox="0 0 25 24"
+                                            style={{ transform: "rotate(-90deg)" }}
+                                          >
+                                            <path
+                                              fillRule="evenodd"
+                                              clipRule="evenodd"
+                                              d="M6.29289 8.8427C6.68342 8.45217 7.31658 8.45217 7.70711 8.8427L12 13.1356L16.2929 8.8427C16.6834 8.45217 17.3166 8.45217 17.7071 8.8427C18.0976 9.23322 18.0976 9.86639 17.7071 10.2569L12 15.964L6.29289 10.2569C5.90237 9.86639 5.90237 9.23322 6.29289 8.8427Z"
+                                              fill="currentColor"
+                                            />
+                                          </svg>
+                                        </span>
+                                      </p>
+                                      <div className="submenu dark:bg-dark absolute top-0 left-full z-50 min-w-[200px] rounded-sm bg-white shadow-lg dark:bg-dark opacity-0 group-hover/submenu:opacity-100 group-hover/submenu:visible invisible transition-opacity duration-300">
+                                        {submenuItem.submenu.map((childItem, childIdx) =>
+                                          childItem.submenu ? (
+                                            // You can recursively render deeper submenus here if needed
+                                            <p
+                                              key={childIdx}
+                                              className="text-dark hover:text-primary block rounded-sm py-2.5 text-sm lg:px-3 dark:text-white/70 dark:hover:text-white"
+                                            >
+                                              {childItem.title}
+                                            </p>
+                                          ) : (
+                                            <Link
+                                              href={childItem.path}
+                                              key={childIdx}
+                                              className="text-dark hover:text-primary block rounded-sm py-2.5 text-sm lg:px-3 dark:text-white/70 dark:hover:text-white"
+                                            >
+                                              {childItem.title}
+                                            </Link>
+                                          )
+                                        )}
+                                      </div>
+                                    </div>
+                                  );
+                                }
+                                return (
+                                  <Link
+                                    href={submenuItem.path}
+                                    key={index}
+                                    className="text-dark hover:text-primary block rounded-sm py-2.5 text-sm lg:px-3 dark:text-white/70 dark:hover:text-white"
+                                  >
+                                    {submenuItem.title}
+                                  </Link>
+                                );
+                              })}
                             </div>
                           </>
                         )}
--- src/components/Header/menuData.tsx
+++ src/components/Header/menuData.tsx
@@ -9,7 +9,7 @@ const menuData: Menu[] = [
   },
   {
     id: 2,
-    title: "About",
+    title: "Know your Dr",
     path: "/about",
     newTab: false,
   },
@@ -34,6 +34,74 @@ const menuData: Menu[] = [
         id: 41,
         title: "About Page",
         path: "/about",
+        newTab: false,
+      },
+      {
+        id: 42,
+        title: "Contact Page",
+        path: "/contact",
+        newTab: false,
+      },
+      {
+        id: 43,
+        title: "Blog Grid Page",
+        path: "/blog",
+        newTab: false,
+      },
+      {
+        id: 44,
+        title: "Blog Sidebar Page",
+        path: "/blog-sidebar",
+        newTab: false,
+      },
+      {
+        id: 45,
+        title: "Blog Details Page",
+        path: "/blog-details",
+        newTab: false,
+      },
+      {
+        id: 46,
+        title: "Sign In Page",
+        path: "/signin",
+        newTab: false,
+      },
+      {
+        id: 47,
+        title: "Sign Up Page",
+        path: "/signup",
+        newTab: false,
+      },
+      {
+        id: 48,
+        title: "Error Page",
+        path: "/error",
+        newTab: false,
+      },
+    ],
+  },
+  {
+    id: 5,
+    title: "Support",
+    newTab: false,
+    submenu: [
+      {
+        id: 41,
+        title: "About Page",
+        submenu: [
+          {
+            id: 47,
+            title: "Sign Up Page",
+            path: "/signup",
+            newTab: false,
+          },
+          {
+            id: 48,
+            title: "Error Page",
+            path: "/error",
+            newTab: false,
+          },
+        ],
         newTab: false,
       },
       {
